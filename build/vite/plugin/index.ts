609a098c5fef4dfef3bd86c5af3955cdf6e1da7a
venson <venson.hunt@gmail.com>
2023-02-08 14:12:59 +0800

    [config] disable mock

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -1,16 +1,15 @@
-import type { Plugin,PluginOption } from 'vite';
+import type { Plugin, PluginOption } from 'vite';
 import Components from 'unplugin-vue-components/vite';
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-
 import { configHtmlPlugin } from './html';
-import { configMockPlugin } from './mock';
+// import { configMockPlugin } from './mock';
 import { configCompressPlugin } from './compress';
 
-export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean, prodMock) {
-  const { VITE_USE_MOCK, VITE_BUILD_COMPRESS, VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE } = viteEnv;
+export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
+  const { VITE_BUILD_COMPRESS, VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE } = viteEnv;
 
   const vitePlugins: (Plugin | Plugin[] | PluginOption[])[] = [
     // have to
@@ -29,7 +28,7 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean, prodMock) 
   vitePlugins.push(configHtmlPlugin(viteEnv, isBuild));
 
   // vite-plugin-mock
-  VITE_USE_MOCK && vitePlugins.push(configMockPlugin(isBuild, prodMock));
+  // VITE_USE_MOCK && vitePlugins.push(configMockPlugin(isBuild, prodMock));
 
   if (isBuild) {
     // rollup-plugin-gzip
